70f01dfd911eb31b0c13b1db03aa134b2aa9c057
Marquis Oliver <marquis.w.oliver@gmail.com>
2020-02-15 18:07:17 -0800

    setting up the buttons

--- script.js
+++ script.js
@@ -10,16 +10,62 @@ var allQuestions = [
   }
 ];
 
+var allAnswers1 = [
+  {
+    answer: "Marquis"
+  },
+  {
+    answer: "Blue"
+  }
+];
+
+var allAnswers2 = [
+  {
+    answer: "Mike"
+  },
+  {}
+];
+
+var allAnswers3 = [
+  {
+    answer: "John"
+  }
+];
+
+var a1 = document.getElementById("button1");
+var a2 = document.getElementById("button2");
+var a3 = document.getElementById("button3");
 var space = document.getElementById("mark");
 var index = 0;
-var onQuestion = document.getElementById("")
+var onQuestion = document.getElementById("");
+
 var startButton = document
   .getElementById("start")
+  .addEventListener("click", function() {
+    space.textContent = allQuestions[index].question;
+    a1.textContent = allAnswers1[index].answer;
+    a2.textContent = allAnswers2[index].answer;
+    a3.textContent = allAnswers3[index].answer;
+    index++;
+  });
+
+var submit1 = document
+  .getElementById("button1")
   .addEventListener("click", function() {
     space.textContent = allQuestions[index].question;
     index++;
   });
 
-var answer = document.getElementById("choice1","choice2","choice3").addEventListener("click",function(){
-    
-});
+var submit2 = document
+  .getElementById("button2")
+  .addEventListener("click", function() {
+    space.textContent = allQuestions[index].question;
+    index++;
+  });
+
+var submit3 = document
+  .getElementById("button3")
+  .addEventListener("click", function() {
+    space.textContent = allQuestions[index].question;
+    index++;
+  });
